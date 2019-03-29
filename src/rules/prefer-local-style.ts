d6391e1a85e12dc7584963bdc561e8b9a6ba7dcd
Vu Tran <vutran@dropbox.com>
2019-03-28 20:51:36 -0700

    add placeholder rule: prefer-local-styles

--- src/rules/prefer-local-style.ts
+++ src/rules/prefer-local-style.ts
@@ -2,14 +2,17 @@ import * as Figma from 'figma-js';
 import { AbstractRule, RuleFailure } from '../rule';
 
 /**
- * All nodes must belong in a Frame.
+ * Prefer local style over hard-coded colors.
  */
 export class Rule extends AbstractRule {
-  static ruleName = 'must-be-in-frame';
+  static ruleName = 'prefer-local-style';
 
   apply(node: Figma.Node): RuleFailure[] {
     if (node.type !== 'DOCUMENT' && node.type !== 'CANVAS') {
       // TODO(vutran) - Implement
+      // node.styles contains a mapping of a mapping of styles
+      // projectData.styles contains styles mapping (id => Figma.Style)
+      // Depends on: https://github.com/jongold/figma-js/pull/15
     }
     return [];
   }
